9b60f0f89abe744b19592a52ddac6337f08ae1c3
Dylan Duario <53376196+DylanRoan@users.noreply.github.com>
2024-09-02 15:50:02 +0400

    adding cors (hopefully)

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const nextConfig = {
         API_URL: process.env.API_URL,
         WEB_URL: process.env.WEB_URL,
         STRIPE_PUBLISHABLE: process.env.STRIPE_PUBLISHABLE
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: process.env.WEB_URL }, 
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" }
+                ]
+            }
+        ]
     }
 };
 
